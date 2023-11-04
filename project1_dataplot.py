5a8d0d662557f545c30657573c983a219ffb0cb3
Sunbot <sok9854@gmail.com>
2023-11-04 20:46:16 +0900

    add dataplot.py

--- project1_dataplot.py
+++ project1_dataplot.py
@@ -1,54 +1,96 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.signal import find_peaks
+import re
 
-# Load the CSV file into a pandas DataFrame
-file_path = './csv/200.0_0.1_1.0.csv'  # 파일 경로를 여러분의 파일 경로로 변경하세요.
-data = pd.read_csv(file_path, header=None, names=['Time', 'Position'])
+# Constants
+FINAL_VALUE = 8.0
+TOLERANCE = 0.02 * FINAL_VALUE
 
-# Redefine the final value as the constant 8
-final_value = 8.0
+# Extract the gains from the file name using regular expressions
+file_name = "1200.0_0.1_1.0.csv"
+match = re.match(r"(\d+\.\d+)_(\d+\.\d+)_(\d+\.\d+)", file_name)
+if match:
+    pgain, igain, dgain = match.groups()
 
-# Rise time (time to reach 90% of the final value for the first time)
-rise_time = data['Time'][data['Position'] >= 0.9 * final_value].iloc[0]
+# Set file paths
+file_path = f'./csv/{file_name}'
+graph_img_path = f"./graph_img/{pgain}_{igain}_{dgain}_response.png"
 
-# Settling time (time after which the position remains within 2% of the final value)
-# Find the last time the position was outside the 2% band of the final value
-settling_index = data[(data['Position'] < 0.98 * final_value) | (data['Position'] > 1.02 * final_value)].index[-1]
-settling_time = data['Time'].iloc[settling_index]
+# Read the data and ITAE value from the CSV
+data = pd.read_csv(file_path, header=None, names=['Time', 'Position'], skipfooter=1, engine='python')
+with open(file_path, 'r') as f:
+    itae_value = float(f.readlines()[-1].strip().split(',')[-1])
 
-# Overshoot (percentage over the final value)
-overshoot_value = data['Position'].max() - final_value
-overshoot_percentage = (overshoot_value / final_value) * 100
+# Calculate the rise time (10% to 90% of the final value)
+rise_time_start = data['Time'][data['Position'] >= 0.1 * FINAL_VALUE].iloc[0]
+rise_time_end = data['Time'][data['Position'] >= 0.9 * FINAL_VALUE].iloc[0]
+rise_time = rise_time_end - rise_time_start
 
-# Plotting the data with annotations for rise time, settling time, and overshoot
+# Find peak time
+peak_time = data['Time'][data['Position'] == data['Position'].max()].iloc[0]
+
+# Find peaks and troughs to determine the settling time
+peaks, _ = find_peaks(data['Position'], height=FINAL_VALUE * (1 - TOLERANCE))
+troughs, _ = find_peaks(-data['Position'], height=-FINAL_VALUE * (1 + TOLERANCE))
+extrema = np.sort(np.concatenate((peaks, troughs)))
+
+# Find the first extremum within the tolerance after the peak time
+settling_time = next((data['Time'][extremum] for extremum in extrema 
+                      if data['Time'][extremum] > peak_time and 
+                      FINAL_VALUE * (1 - TOLERANCE) <= data['Position'][extremum] <= FINAL_VALUE * (1 + TOLERANCE)),
+                     None)
+
+# Calculate steady state error (SSE)
+steady_state_error = FINAL_VALUE - data['Position'].iloc[-1]
+
+# Plot the motor position response
 plt.figure(figsize=(12, 6))
-
-# Plot the response
 plt.plot(data['Time'], data['Position'], label='Motor Position', color='blue')
 
-# Annotate the rise time
-rise_time_position = data['Position'][data['Time'] == rise_time].values[0]
-plt.plot([0, rise_time], [rise_time_position, rise_time_position], color='green', linestyle='--')
-plt.plot([rise_time, rise_time], [0, rise_time_position], color='green', linestyle='--')
-plt.text(rise_time, 0, f'Rise Time: {rise_time:.2f}s', verticalalignment='bottom', horizontalalignment='right', color='green')
+# Annotate rise time
+plt.annotate(f'10-90% Rise Time: {rise_time:.2f}s', 
+             xy=(rise_time_end, FINAL_VALUE * 0.9), 
+             xytext=(rise_time_end, FINAL_VALUE * 0.95), 
+             arrowprops=dict(facecolor='green', shrink=0.05), 
+             color='green')
 
-# Annotate the settling time
-settling_time_position = data['Position'][data['Time'] == settling_time].values[0]
-plt.plot([settling_time, settling_time], [0, settling_time_position], color='red', linestyle='--')
-plt.text(settling_time, 0, f'Settling Time: {settling_time:.2f}s', verticalalignment='bottom', horizontalalignment='right', color='red')
+# Annotate settling time
+if settling_time:
+    plt.annotate(f'Settling Time: {settling_time:.2f}s', 
+                 xy=(settling_time, FINAL_VALUE), 
+                 xytext=(settling_time + 0.2, FINAL_VALUE * 0.5), 
+                 arrowprops=dict(facecolor='red', shrink=0.05), 
+                 color='red')
 
-# Annotate the overshoot
-overshoot_time = data['Time'][data['Position'] == data['Position'].max()].values[0]
-plt.plot([overshoot_time, overshoot_time], [final_value, data['Position'].max()], color='purple', linestyle='--')
-plt.scatter([overshoot_time], [data['Position'].max()], color='purple')
-plt.text(overshoot_time, final_value, f'Overshoot: {overshoot_percentage:.2f}%', verticalalignment='bottom', horizontalalignment='right', color='purple')
+# Annotate overshoot
+overshoot_value = data['Position'].max() - FINAL_VALUE
+overshoot_time = data['Time'][data['Position'] == data['Position'].max()].iloc[0]
+overshoot_percentage = (overshoot_value / FINAL_VALUE) * 100
+plt.annotate(f'Overshoot: {overshoot_percentage:.2f}%', 
+             xy=(overshoot_time, data['Position'].max()), 
+             xytext=(overshoot_time + 0.1, data['Position'].max() + 0.5), 
+             arrowprops=dict(facecolor='purple', shrink=0.05), 
+             color='purple')
 
-# Final value line
-plt.hlines(final_value, 0, data['Time'].iloc[-1], colors='orange', linestyles='--', label='Final Value (8.0)')
+# Annotate steady state error
+plt.annotate(f'SSE: {steady_state_error:.2f}', 
+             xy=(data['Time'].iloc[-1], data['Position'].iloc[-1]), 
+             xytext=(data['Time'].iloc[-50], data['Position'].iloc[-1] + 0.5), 
+             arrowprops=dict(facecolor='orange', shrink=0.05), 
+             color='orange')
 
-plt.title('Motor Position Response Over Time with Annotations')
+# Add final value line, title, labels, and legend
+plt.axhline(y=FINAL_VALUE, color='orange', linestyle='--', label='Final Value')
+plt.title(f'Motor Position Response (PGain: {pgain}, IGain: {igain}, DGain: {dgain}, ITAE: {itae_value:.2f})')
 plt.xlabel('Time (seconds)')
 plt.ylabel('Position')
 plt.grid(True)
-plt.legend()
+plt.legend(loc='lower right')
+plt.xlim(left=0)
+plt.ylim(bottom=0)
+
+# Save the figure and display the plot
+plt.savefig(graph_img_path, bbox_inches='tight')
 plt.show()
